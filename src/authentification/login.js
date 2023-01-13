e17d2da446761b5409d9aa3c4559fcdf76bb0fb3
wen <71599550+Wen-Miin@users.noreply.github.com>
2023-01-14 00:55:02 +0100

    maj login

--- src/authentification/login.js
+++ src/authentification/login.js
@@ -1,5 +1,5 @@
 const db = resquire('../mongo')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const { jwtSecretKey } = require('../config')
 
@@ -14,22 +14,7 @@ const router = express.Router()
 
 // on génère un hash du mot de passe
 async function generateHashPassword(password) {
-    await bcrypt.hash(password, salt)
-}
-
-// génération d'un token avec le username et la clé secrète
-function generateToken(username) {
-    return jwt.sign({ username }, jwtSecretKey)
-}
-
-// récupération du token
-function getToken(req) {
-    return _.replace(req.headers.authorization, 'Bearer ', '')
-}
-
-// génération d'un token avec le username et la clé secrète et le role
-function generateTokenWithRole(username, role) {
-    jwt.sign({ username, role }, jwtSecretKey)
+    await bcrypt.hash(password, saltRounds)
 }
 
 // vérification du token
@@ -50,33 +35,5 @@ async function VerifyToken(token) {
     })
 }
 
-// on dit a nos routes d'utiliser a travers un préfix une autre instance d'express router.
-router.post('/login', async (req, res) => {
-    const { username, password } = req.body // on récupère le username et le password dans le body de la requête
 
-    console.log(`User ${username} login attempt.`)
 
-    const user = await db.users.findUserByUsername(username) // on récupère les informations de l'utilisateur
-
-    if (!userExistsByToken(getToken(req))) {
-        // si l'utilisateur n'existe pas
-        const errorMessage = `User ${username} not found.`
-        console.log(errorMessage)
-        return res.status(404).json({ message: errorMessage })
-    }
-
-    const passwordMatch = await bcrypt.compare(password, user.password) // on vérifie que le mot de passe correspond
-    if (!passwordMatch) {
-        // si le mot de passe ne correspond pas
-        const errorMessage = `Wrong password for user ${username}.`
-        console.log(errorMessage)
-        return res.status(401).json({ message: errorMessage })
-    }
-
-    const token = generateToken(username) // on génère un token
-
-    await db.users.updateUserToken(username, token) // on met a jour le token de l'utilisateur
-    console.log(`User ${username} logged in.`)
-    console.log(`User ${username} token: ${token}`)
-    return res.json({ token }) // on renvoie le token au client
-})
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const _ = require('lodash')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 const db = require('../mongo')
-//const { jwtSecretKey } = require('../config')
-const users = require('../mongo/schema/users')
+const { jwtSecretKey } = require('../config')
 
 // on créer une instance d'express router
 const router = express.Router()
@@ -60,6 +60,64 @@ router.get('/me', async (req, res) => {
     const user = await db.users.findUserByToken(token) // on récupère les informations de l'utilisateur
 
     res.json(user)
+})
+
+// on dit a nos routes d'utiliser a travers un préfix une autre instance d'express router.
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body // on récupère le username et le password dans le body de la requête
+
+    console.log(`User ${username} login attempt.`)
+
+    const user = await db.users.findUserByUsername(username) // on récupère les informations de l'utilisateur
+    const userExists = await db.users.userExistsByUsername(username)
+
+    if (!userExists) {
+        // si l'utilisateur n'existe pas
+        const errorMessage = `User ${username} not found.`
+        console.log(errorMessage)
+        return res.status(404).json({ message: errorMessage })
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password) // on vérifie que le mot de passe correspond
+    if (!passwordMatch) {
+        // si le mot de passe ne correspond pas
+        const errorMessage = `Wrong password for user ${username}.`
+        console.log(errorMessage)
+        return res.status(401).json({ message: errorMessage })
+    }
+
+    console.log(`User ${username} logged in.`)
+    console.log(`User ${username} token: ${user.token}`)
+
+    return res.json({
+        username: user.username,
+        role: user.role,
+        token: user.token,
+    }) // on renvoie le token au client
+})
+
+router.post('/create-assignment', async (req, res) => {
+    const { username } = await db.users.findUserByToken(getToken(req)) // on récupère le token et on récupère le nom d'utilisateur par son token
+
+    const { nom, auteur, matiere, dateDeRendu, note, rendu, remarque } =
+        req.body // on récupère le title, description et rendu dans le body de la requête
+
+    const assignment = await db.assignments.createAssignment(
+        // on crée l'assignment
+        {
+            nom,
+            auteur,
+            matiere,
+            dateDeRendu,
+            note,
+            rendu,
+            remarque,
+        }
+    )
+
+    console.log('User ${username} created assignment ${nom}.')
+
+    res.json(assignment) // on renvoie l'assignment au client
 })
 
 module.exports = router
